01af13ab50326a52cda1a813aa5169cc2ce089cd
Apple12345123 <pgh3621@naver.som>
2024-06-10 15:46:33 +0900

    안드로이드 ch14_etc

---
+++ ch14_etc/src/main/java/com/busani/ch14_etc/CardviewActivity.kt
@@ -0,0 +1,20 @@
+package com.busani.ch14_etc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CardviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cardview)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
